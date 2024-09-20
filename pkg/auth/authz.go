9983280ef424f92260fbeed8025ec553e3e65bbb
jami1024 <996013797@qq.com>
2024-09-20 17:40:16 +0800

    增加获取用户详情信息

--- pkg/auth/authz.go
+++ pkg/auth/authz.go
@@ -1,3 +1,8 @@
+// Copyright 2023 jami1024 &lt;996013797@qq.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/jami1024/miniblog.
+
 package auth
 
 import (
